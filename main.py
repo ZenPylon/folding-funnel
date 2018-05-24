8562078a0eab7ee472c336b6d0604e4522af72f8
zenpylon <ajdecker1022@gmail.com>
2018-05-24 15:47:06 -0500

    Refactor remove hetero atoms

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from Bio.PDB import *
 from math import pi
 
+def remove_hetero(model, chain):
+    """Remove heteroatoms from a chain and return the new residue_list"""
+    residue_list = [res for res in chain.get_residues()]
+    hetero_residue_ids = []
+    for res in residue_list:
+        if res.id[0] != ' ':
+            hetero_residue_ids.append(res.id)
+
+    print(f'Number of residues in chain: {len(residue_list)}')
+    print(f'Removing {len(hetero_residue_ids)} hetero residues')
+    [chain.detach_child(hetero_id) for hetero_id in hetero_residue_ids]
+
+    # Update residues after removing hetero residue
+    residues = structure.get_residues()
+    residue_list = [res for res in residues]
+    print(f'Chain now has {len(residue_list)} residues')
+    return residue_list
+
+
 # BioPython setup
 parser = PDBParser()
 ppb = PPBuilder()
@@ -16,21 +35,9 @@ if len(models) != 1 or len(chains) != 1:
 # Remove hetero residues (such as solvent)
 model = models[0]
 chain = chains[0]
-residue_list = [res for res in chain.get_residues()]
-hetero_residue_ids = []
+residue_list = remove_hetero(model, chain)
+print(len(residue_list))
 
-for res in residue_list:
-    if res.id[0] != ' ':
-        hetero_residue_ids.append(res.id)
-
-print(f'Number of residues in chain: {len(residue_list)}')
-print(f'Removing {len(hetero_residue_ids)} hetero residues')
-[chain.detach_child(hetero_id) for hetero_id in hetero_residue_ids]
-
-# Update residues after removing hetero residues
-residues = structure.get_residues()
-residue_list = [res for res in residues]
-print(f'Chain now has {len(residue_list)} residues')
 
 # Build a polypeptide from our structure and get the torsion angles
 polypeptides = ppb.build_peptides(structure)
@@ -46,27 +53,27 @@ for atom in residue_list[0].get_atoms():
     print(atom.get_vector())
 residue = residue_list[0]
 
-# From Biopython FAQ
-nitro = residue['N'].get_vector()
-carbon = residue['C'].get_vector()
-carbon_a = residue['CA'].get_vector()
+# # From Biopython FAQ
+# nitro = residue['N'].get_vector()
+# carbon = residue['C'].get_vector()
+# carbon_a = residue['CA'].get_vector()
 
-# center at origin
-nitro = nitro - carbon_a
-carbon = carbon - carbon_a
+# # center at origin
+# nitro = nitro - carbon_a
+# carbon = carbon - carbon_a
 
-# find rotation matrix that rotates n -120 degrees along the ca-c vector
-rot = rotaxis(-pi * 120.0/180.0, carbon)
+# # find rotation matrix that rotates n -120 degrees along the ca-c vector
+# rot = rotaxis(-pi * 120.0/180.0, carbon)
 
-# apply rotation to ca-n vector
-cb_at_origin = nitro.left_multiply(rot)
+# # apply rotation to ca-n vector
+# cb_at_origin = nitro.left_multiply(rot)
 
-# put on top of ca atom
-cb = cb_at_origin + carbon_a
+# # put on top of ca atom
+# cb = cb_at_origin + carbon_a
 
-print(f'rotated {cb}')
+# print(f'rotated {cb}')
 
-for atom in residue_list[0].get_atoms():
-    print(atom.get_coord())
-    print(atom.get_vector())
+# for atom in residue_list[0].get_atoms():
+#     print(atom.get_coord())
+#     print(atom.get_vector())
 
