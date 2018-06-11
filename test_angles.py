b20ad78b06696271eff597291e5e4da80bd1ddba
zenpylon <ajdecker1022@gmail.com>
2018-06-10 21:30:48 -0500

    Expand and simplify test_angles...still failing

--- test_angles.py
+++ test_angles.py
@@ -10,6 +10,7 @@ from angles import rot_atom, rot_backbone
 structure, residue_list, polypeptide = main_load('ubiq', '1ubq.pdb')
 torsion_angles = polypeptide.get_phi_psi_list()
 
+
 def test_coords_equal():
     """
     Ensure sure that after a phi or psi angle is updated, the relative 
@@ -19,7 +20,7 @@ def test_coords_equal():
     nitro_pos = res['N'].get_vector()
     carbon_pos = res['C'].get_vector()
     carbon_a_pos = res['CA'].get_vector()
-    
+
     # Get all atoms in the side-chain (i.e. those not in the backbone)
     backbone = [res['N'], res['C'], res['CA'], res['O']]
     sidechain = [atom for atom in res.get_atoms() if atom not in backbone]
@@ -37,12 +38,18 @@ def test_coords_equal():
 
     # isclose(a, b)
 
+
 def test_rot_atom():
-    res0 = polypeptide[0]
-    res1 = polypeptide[1]
+    res0 = polypeptide[1]
+    res1 = polypeptide[2]
+    res0['N'].coord = Vector(0, 0, 0)
+    res0['CA'].coord = Vector(3, 0, 0)
+    res0['C'].coord = Vector(3, 1, 0)
+    res1['N'].coord = Vector(3, 0, -1)
     res1_n = res1['N'].get_vector()
+    
     new_coord = rot_atom(0, (res0['N'], res0['CA'], res0['C'], res1['N']))
-    print(new_coord)
-    print(res1_n)
-    no_rotation = np.array_equal(new_coord.get_array(), res1_n.get_array())
+    print(f'rotated vector {new_coord.get_array()}')
+    print(f'original vector {res1_n.get_array()}')
+    no_rotation = np.allclose(new_coord.get_array(), res1_n.get_array())
     assert(no_rotation)
