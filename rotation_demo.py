6409ba880a9c2572c2d529d8b72b57b71489d424
zenpylon <ajdecker1022@gmail.com>
2018-06-15 21:41:55 -0500

    Add bonds as lines, tidy code

--- rotation_demo.py
+++ rotation_demo.py
@@ -9,10 +9,13 @@ from matplotlib import animation
 
 # TODO - make 3d plot with different angles
 # Generate coordinates in script, import into folding_funnel notebook
+num_res = 4
+num_atoms = num_res * 3
+
 
 def get_backbone(polypeptide):
-    num_res = 4
-    coords = np.zeros((3, num_res * 3))
+
+    coords = np.zeros((3, num_atoms))
     for index, res in enumerate(polypeptide[0:num_res]):
         coords[:, index * 3] = res['N'].coord
         coords[:, index * 3 + 1] = res['CA'].coord
@@ -20,15 +23,16 @@ def get_backbone(polypeptide):
 
     return coords
 
+
 structure, residue_list, polypeptide = main_load('ubiq', '1ubq.pdb')
-print(polypeptide[0]['N'].coord)
 backbone = get_backbone(polypeptide)
-print(backbone)
 fig = plt.figure()
 ax = p3.Axes3D(fig)
+ax.scatter3D(backbone[0, :], backbone[1, :], backbone[2, :], s=100, c=(0, 0, 0))
+lines = [ax.plot(backbone[0, :],
+                 backbone[1, :],
+                 backbone[2, :],
+                 c='b')[0] for index in range(num_atoms - 1)]
 
-ax.scatter3D(backbone[0, :], backbone[1, :], backbone[2, :], s=100)
 plt.show()
-# x = np.linspace(0, 5)
-# y = np.sin(x)
 # animation.FuncAnimation(fig, )
