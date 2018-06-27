dc1ddc990ae5570808e4e38fd2edd656921ab117
zenpylon <ajdecker1022@gmail.com>
2018-06-27 08:56:56 -0500

    Add init sequence to main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from simtk.openmm.app import PDBFile, ForceField, Modeller, PME, HBonds
 from simtk.unit import kelvin, nanometer, picosecond, picoseconds
 import chemcoord as cc
 import numpy as np
+import pandas as pd
 import util
 
 np.random.seed(20)
@@ -10,17 +11,17 @@ np.random.seed(20)
 # Don't worry about hydrogen atoms - we'll add them to the PDB model later
 ubiq_zmat = '1ubq.xyz'
 ubiq_pdb = '1ubq.pdb'
-structure, residue_list, polypeptide = util.main_load('ubiq', ubiq_pdb)
+# structure, residue_list, polypeptide = util.main_load('ubiq', ubiq_pdb)
 
-for torsions in polypeptide.get_phi_psi_list():
-    angle0 = None
-    angle1 = None
-    if torsions[0] is not None:
-        angle0 = torsions[0] / pi * 180
-    if torsions[1] is not None:
-        angle1 = torsions[1] / pi * 180
+# for torsions in polypeptide.get_phi_psi_list():
+#     angle0 = None
+#     angle1 = None
+#     if torsions[0] is not None:
+#         angle0 = torsions[0] / pi * 180
+#     if torsions[1] is not None:
+#         angle1 = torsions[1] / pi * 180
 
-    print((angle0, angle1))
+#     print((angle0, angle1))
 
 
 def get_modeller(pdb_file: str):
@@ -38,9 +39,10 @@ def get_zmat(zmat_filename: str):
     Args:
         zmat_filename - the file name in firebase
     """
-    zmat_file = load_from_firebase(zmat_filename)
-    zmat = cc.ZMat.read_zmat(zmat_file)
-    return zmat
+    # zmat_file = load_from_firebase(zmat_filename)
+    # zmat = cc.ZMat.read_zmat(zmat_file)
+    # return zmat
+    return None
 
 
 def calculate_zmat(modeller: Modeller):
@@ -90,15 +92,8 @@ def save_starting_zmat(zmat_name: str, zmat: cc.Zmat):
         zmat_name - name of file to write
         zmat - the zmat data to save
     """
-    save_to_firebase(zmat)
-
-
-def load_starting_zmat(pdb_name: str):
-    """
-    Loads the initial zmat data (unmodified internal coordinates 
-    of the PDB file) from firebase
-    """
-    print('TODO')
+    print('TODO - save zmat')
+    # save_to_firebase(zmat)
 
 
 def get_torsion_indices(zmat):
@@ -108,22 +103,62 @@ def get_torsion_indices(zmat):
     Args:
         zmat: the zmatrix specifying the molecule
     """
-    print('TODO')
+    phi_indices = []
+    psi_indices = []
 
+    for i in range(len(zmat.index)):
+        b_index = zmat.loc[i, 'b'] 
+        a_index = zmat.loc[i, 'a'] 
+        d_index = zmat.loc[i, 'd']
+
+        if isinstance(b_index, str) or isinstance(a_index, str) or isinstance(d_index, str):
+            continue
+
+        # Psi angles
+        if (zmat.loc[i, 'atom'] == 'N') & \
+                (zmat.loc[b_index, 'atom'] == 'CA') & \
+                (zmat.loc[a_index, 'atom'] == 'C') & \
+                (zmat.loc[d_index, 'atom'] == 'N'):
+            psi_indices.append(i)
+
+        elif (zmat.loc[i, 'atom'] == 'N') & \
+                (zmat.loc[b_index, 'atom'] == 'C') & \
+                (zmat.loc[a_index, 'atom'] == 'CA') & \
+                (zmat.loc[d_index, 'atom'] == 'N'):
+            psi_indices.append(i)
+        
+        elif (zmat.loc[i, 'atom'] == 'C') & \
+                (zmat.loc[b_index, 'atom'] == 'N') & \
+                (zmat.loc[a_index, 'atom'] == 'CA') & \
+                (zmat.loc[d_index, 'atom'] == 'C'):
+            phi_indices.append(i)
+
+        elif (zmat.loc[i, 'atom'] == 'C') & \
+                (zmat.loc[b_index, 'atom'] == 'CA') & \
+                (zmat.loc[a_index, 'atom'] == 'N') & \
+                (zmat.loc[d_index, 'atom'] == 'C'):
+            phi_indices.append(i)
+    
+    return np.array([phi_indices, psi_indices]).T
 
 def init_model():
     modeller = get_modeller(ubiq_pdb)
-    starting_zmat = get_zmat(ubiq_zmat)
+    zmat = get_zmat(ubiq_zmat)
     if zmat is None:
-        starting_zmat = calculate_zmat(modeller)
-        save_starting_zmat(zmat)
+        zmat = calculate_zmat(modeller)
+        save_starting_zmat(ubiq_zmat, zmat)
 
-    changing_zmat = starting_zmat.copy()
-    starting_torsions = get_torsion_indices(starting_zmat)
-    offsets = np.random.choice([0, 0, -1, 1], torsions.shape)
-    run_sequence(starting_torsions, offsets)
+    torsion_indices = get_torsion_indices(zmat)
+    print(torsion_indices)
+    starting_torsions = np.array([zmat.loc[torsion_indices[:, 0], 'dihedral'],
+                                  zmat.loc[torsion_indices[:, 1], 'dihedral']]).T
+
     
-def run_sequence(starting_torsions, offsets, offset_size=4, num_configs=30):
+    offsets = np.random.choice([0, 0, -1, 1], starting_torsions.shape)
+    run_sequence(zmat, starting_torsions, torsion_indices, offsets)
+
+
+def run_sequence(zmat: cc.Zmat, starting_torsions, torsion_indices, offsets, offset_size=4, num_configs=3):
     """
     Args:
         pdb_name: name field in firebase
@@ -131,19 +166,22 @@ def run_sequence(starting_torsions, offsets, offset_size=4, num_configs=30):
         offset_size: amount to deviate on each configuration
         num_configs: number of configurations to try
     """
-   
-    print('\n\ntorsions\n\n')
-    print(torsions)
-    print('offsets')
-    print(offsets)
 
+    print(f'\n\nstarting torsions {starting_torsions.shape}\n')
+    print(starting_torsions)
+    print(f'\n\noffsets {offsets.shape}\n')
+    print(offsets)
     # Modify (on average) half the torsion angles
     for i in range(num_configs):
-        changing_zmat.safe_loc[torsions[:, 0], 'dihedral'] = \
-                starting_torsions[:, 0] + offsets[:, 0] + (i * offset_size)
-        changing_zmat.safe_loc[torsions[:, 1], 'dihedral'] = \
-                starting_torsions[:, 1] + offsets[:, 1] + (i * offset_size)
+        zmat.safe_loc[torsion_indices[:, 0], 'dihedral'] = \
+            starting_torsions[:, 0] + (offsets[:, 0] * i * offset_size)
+        zmat.safe_loc[torsion_indices[:, 1], 'dihedral'] = \
+            starting_torsions[:, 1] + (offsets[:, 1] * offset_size * i)
+        print(zmat.loc[torsion_indices[:, 0], 'dihedral'])
+        print(zmat.loc[torsion_indices[:, 1], 'dihedral'])
 
+
+init_model()
 
 # TODO - test by changing angles more and more, and plot vs. RMSD distance
 #        Expectation is that it RMSD should increase as angle distance increases
@@ -174,7 +212,7 @@ def run_sequence(starting_torsions, offsets, offset_size=4, num_configs=30):
 #    and get absolute phi / psi coordinates (calculate native angles + deltas).
 # 2. Convert from angle-space to cartesian coordinates.
 # 3. Pass coordinates to OpenMM (how?).
-    # Use topology.addChain, topology.addResidue(), etc based on constructed structure
+# Use topology.addChain, topology.addResidue(), etc based on constructed structure
 # 4. Add solvent to model with OpenMM.
 # 5. Run energy minimization on model.
 # 6. Pass final energy value and coordinates to BioPython, and reset atom coords.
