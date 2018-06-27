4429136352562be5d97a6556cd9d65a38f9bf3c0
zenpylon <ajdecker1022@gmail.com>
2018-06-27 10:14:55 -0500

    Remove outdated docs

--- src/main.py
+++ src/main.py
@@ -187,36 +187,3 @@ def run_sequence(zmat: cc.Zmat, starting_torsions, torsion_indices, offsets, off
 
 init_model()
 
-# TODO - test by changing angles more and more, and plot vs. RMSD distance
-#        Expectation is that it RMSD should increase as angle distance increases
-
-# Construct backbone from angles and distances
-
-# THEN
-# 1. Save PDB with minor alterations
-# 1. Calculate RMSD.  Should be small
-# 1. Plot superimposed or side-by-side structures.  Get visual confirmation
-# of similarity
-
-# THEN:
-# 1. Make increasingly large deviations from loaded structure
-# 1. Calculate RMSD
-# 1. Plot Angle distance vs RMSD.  Should be positive correlation
-
-
-
-# TODO: The Algorithm:
-#
-# 1. Specify all phi / psi angle deltas (magnitude M) relative to native structure
-#    and get absolute phi / psi coordinates (calculate native angles + deltas).
-# 2. Convert from angle-space to cartesian coordinates.
-# 3. Pass coordinates to OpenMM (how?).
-# Use topology.addChain, topology.addResidue(), etc based on constructed structure
-# 4. Add solvent to model with OpenMM.
-# 5. Run energy minimization on model.
-# 6. Pass final energy value and coordinates to BioPython, and reset atom coords.
-# 7. Recalculate phi / psi angles from coordinates.
-# 8. Calculate distance in angle-space from starting point (in this case,
-#    native structure) and calculate delta in energy level.
-# 9. Output distance and energy-delta to file (i.e track progress).
-# 10. Repeat steps 1-9, increasing delta magnitude M each time.
