de08815097bc31dfe20e0ee6e523c1798f24d4da
zenpylon <ajdecker1022@gmail.com>
2018-06-28 21:10:06 -0500

    GCP messed that one up D:

--- src/broker.py
+++ src/broker.py
@@ -1,13 +1,21 @@
 from google.cloud import storage
+from google.cloud.pubsub_v1.gapic.publisher_client import PublisherClient
+from google.cloud.pubsub_v1.gapic.subscriber_client import SubscriberClient
 from molecule_util import get_modeller, calculate_zmat
 
 bucket_name = 'funnel-folding.appspot.com'
+
+publisher = PublisherClient()
+subscriber = SubscriberClient()
+project_id = 'funnel_folding'
+
 pdb_file = '1ubq.pdb'
 client = storage.Client()
 bucket = client.bucket(bucket_name)
 pdb = None
 modeller = None
 
+# Load the PDB file and construct the modeller and zmatrix
 try:
     with open(f'data/{pdb_file}', 'wb+') as f:
         print('Downloading PDB file...')
@@ -24,3 +32,13 @@ except Exception as e:
 modeller = get_modeller(f'data/{pdb_file}')
 zmat = calculate_zmat(modeller)
 print(zmat)
+# Listen for worker events
+subscriber.create_subscription(
+    f'projects/{project_id}/topics/init_requests',
+    f'projects/{project_id}/subscriptions/broker_init_request',
+)
+
+# Init worker processes
+
+
+
